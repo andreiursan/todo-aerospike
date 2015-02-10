a4a064ae5a7c892f54389af855e7598a0605ced7
Andrei Ursan <hello@andreiursan.com>
2015-02-10 21:04:17 +0100

    defined the aerospike-component

--- src/todo_aerospike/component/aerospike_component.clj
+++ src/todo_aerospike/component/aerospike_component.clj
@@ -1,13 +1,25 @@
 (ns todo-aerospike.component.aerospike-component
   (:require [com.stuartsierra.component :as component])
-  (:import '(com.aerospike.client AerospikeClient Info)
-           '(com.aerospike.client.policy Policy WritePolicy)
-           '(com.aerospike.client.cluster Node)))
+  (:import [com.aerospike.client AerospikeClient Info]
+           [com.aerospike.client.policy Policy WritePolicy]
+           [com.aerospike.client.cluster Node]))
 
-(defrecord AerospikeComponent []
+(defn connect-to-database [host port]
+  (new AerospikeClient host port))
+
+(defrecord AerospikeComponent [host port db]
+  ;; Implement the Lifecycle protocol
   component/Lifecycle
-  (start [this] this)
-  (stop [this] this))
 
-(defn aerospike-component []
-  (->AerospikeComponent))
+  (start [component]
+    (println ";; Starting database")
+    (let [db (connect-to-database host port)]
+      (assoc component :db db)))
+
+  (stop [component]
+    (println ";; Stopping database")
+    (.close db)
+    (assoc component :db nil)))
+
+(defn aerospike-component [host port]
+  (map->AerospikeComponent {:host host :port port}))
