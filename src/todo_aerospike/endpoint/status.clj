9640055d04b99acfb46906348d8d15cac7f5c5cd
Andrei Ursan <hello@andreiursan.com>
2015-02-11 18:45:36 +0100

    cleaned a bit the status endpoint

--- src/todo_aerospike/endpoint/status.clj
+++ src/todo_aerospike/endpoint/status.clj
@@ -2,10 +2,10 @@
   (:require [compojure.core :refer :all])
   (:import [com.aerospike.client Info]))
 
+(defn node-status [conn]
+  (let [first-node (.. conn getNodes first)]
+    (str (. Info request nil first-node))))
+
 (defn status-endpoint [{db :db}]
   (routes
-   (GET "/status" []
-        (let [conn (:conn db)
-              first-node (first (.getNodes conn))]
-          (str (. Info request nil first-node))))))
-
+   (GET "/status" [] (node-status (:conn db)))))
