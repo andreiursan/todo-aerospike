1465f849876b07b469a77e2522a7f202e9a6adba
Andrei Ursan <hello@andreiursan.com>
2015-02-22 00:23:42 +0100

    refactored utils/db.clj, WIP not yet in the best shape

--- src/todo_aerospike/domain/utils/db.clj
+++ src/todo_aerospike/domain/utils/db.clj
@@ -3,6 +3,8 @@
            [com.aerospike.client.cluster Node]
            [com.aerospike.client.policy WritePolicy]))
 
+(def write-policy (new WritePolicy))
+
 (defn nodes-statuses [conn]
   "Returns a vector with the status of
   each node in the Aerospike Cluster
@@ -10,15 +12,24 @@
   (let [nodes (.getNodes conn)]
     (map #(. Info request nil %) nodes)))
 
-(def key-namespace "todo-aerospike")
-(def write-policy (new WritePolicy))
+(defn- create-key [keymap]
+  (new Key (:namespace keymap) (:set keymap) (:uuid keymap)))
 
-(defn uuid [] (str (java.util.UUID/randomUUID)))
+(defn- create-bin [binmap]
+  (new Bin (:column binmap) (:value binmap)))
 
-(defn put [conn record]
-  "[C]RUD creates a record"
-  (let [bin (new Bin (:column record) (:value record))
-        key (new Key key-namespace (:set record) (uuid))]
-        (. conn put write-policy key (into-array [bin]))))
+(defn upsert [conn keymap binmap]
+  "[C]R[U]D upserts a record"
+  (let [key (create-key keymap)
+        bin (create-bin binmap)]
+    (. conn put write-policy key (into-array [bin]))))
 
+(defn get-record [conn keymap]
+  "C[R]UD retrieves the record by its key"
+  (let [key (create-key keymap)]
+    (. conn get nil key)))
 
+(defn delete [conn keymap]
+  "CRU[D] deletes a record"
+  (let [key (create-key keymap)]
+    (. conn delete write-policy key)))
