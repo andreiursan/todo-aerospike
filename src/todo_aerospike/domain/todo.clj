32af1b090f932376726ba37afa05c773bc447c96
Andrei Ursan <hello@andreiursan.com>
2015-03-08 19:17:28 +0100

    exploring ways to crud the todo items - although due to the connection I feel the need of creating a component for it

--- src/todo_aerospike/domain/todo.clj
+++ src/todo_aerospike/domain/todo.clj
@@ -1,3 +1,15 @@
 (ns todo-aerospike.domain.todo
   (:require [todo-aerospike.domain.utils.db :as db]))
 
+(def todo-set "todos")
+(def todo-namespace "test")
+
+(defn create-key
+  ([] create-key (str (java.util.UUID/randomUUID)))
+  ([uuid] ({:namespace todo-namespace :set todo-set :uuid uuid})))
+
+(defn add [conn todo]
+  (db/upsert conn (create-key) todo))
+
+(defn delete [conn uuid]
+  (db/delete conn (create-key uuid)))
